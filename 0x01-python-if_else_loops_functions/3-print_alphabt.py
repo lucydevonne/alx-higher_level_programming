5566aff4948eac1cb71af32de91eaabd1fbeea52
lucydevonne <wambuilucie7@gmail.com>
2022-11-29 01:32:18 -0800

    updated

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for alpha_letters in range(97, 122):
+      if alpha_letters == ord(e) or alpha_letters == ord(q):
+          continue
+      print("{:c}".format(alpha_letters), end="")
