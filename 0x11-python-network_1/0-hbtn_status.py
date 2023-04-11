7760632c732b0e6d8789099d4361e74700adedd9
lucydevonne <wambuilucie7@gmail.com>
2023-04-11 15:04:14 -0700

    py network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python3
-"""Python script that fetches the given url"""
-
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
+
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        html = response.read()
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body)))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
